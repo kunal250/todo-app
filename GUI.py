ae74821e2ba40945e55084a4aab6e51ced2d731b
kunal <kunalrai67890@gmail.com>
2022-12-08 14:19:36 +0530

    Implement the complete and exit button

--- GUI.py
+++ GUI.py
@@ -7,11 +7,15 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todo(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 
 window = sg.Window('MY To-Do App',
                    layout=[[label],
-                           [input_box, add_button], [list_box, edit_button]],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=("Helvetica", 20))
 
 while True:
@@ -34,6 +38,15 @@ while True:
             todos[index] = new_todo
             functions.write_todo(todos)
             window['todos'].update(values=todos)
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todo()
+            todos.remove(todo_to_complete)
+            functions.write_todo(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            exit()
 
         case 'todos':
             window['todo'].update(value=values['todos'][0])
